6a584a38fd3e5f245f6e546a1b0f534481f3e5f1
Bruno Nonogaki <brunono@gmail.com>
2025-11-04 21:39:58 -0300

    finishing  status route

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,7 +1,37 @@
 import database from "infra/database.js";
 
+async function get_postgres_version() {
+  const result = await database.query("SHOW server_version");
+  return result.rows[0].server_version;
+}
+
+async function get_postgres_max_connections() {
+  const result = await database.query(
+    "SHOW max_connections",
+  );
+  return parseInt(result.rows[0].max_connections);
+}
+
+async function get_postgres_used_connections() {
+  // Sem proteção de SQL Injection:
+  // `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${process.env.POSTGRES_DB}';`
+  const result = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB]
+  });
+  return result.rows[0].count;
+}
+
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 AS sum;");
-  console.log(result.rows)
-  response.status(200).json(result);
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database : {
+        version: await get_postgres_version(),
+        max_connections: await get_postgres_max_connections(),
+        opened_connections: await get_postgres_used_connections(),
+      }
+    }
+  });
 }
