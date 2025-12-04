934b9e3d85e04a71c8f921f32f54509322fa58e0
Bruno Nonogaki <brunono@gmail.com>
2025-12-04 18:59:09 -0300

    refactor package.json and tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -10,9 +10,12 @@ describe("POST to /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("First run", async () => {
-        const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response1.status).toBe(201);
 
         const responseBody1 = await response1.json();
@@ -20,9 +23,12 @@ describe("POST to /api/v1/migrations", () => {
         expect(responseBody1.length).toBeGreaterThan(0);
       });
       test("Second run", async () => {
-        const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response2.status).toBe(200);
 
         // Segundo POST
