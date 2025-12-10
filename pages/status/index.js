425c5a16d271766c91d7ec95161adfdb575a0684
Bruno Nonogaki <brunono@gmail.com>
2025-12-09 22:14:18 -0300

    fix: change url path to work in vercel

--- pages/status/index.js
+++ pages/status/index.js
@@ -18,13 +18,9 @@ export default function StatusPage() {
 }
 
 function UpdatedAt() {
-  const { isLoading, data } = useSWR(
-    "http://localhost:3000/api/v1/status",
-    fetchAPI,
-    {
-      refreshInterval: 2000,
-    },
-  );
+  const { isLoading, data } = useSWR("api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
 
   let updatedAtText = "Carregando...";
 
@@ -36,13 +32,9 @@ function UpdatedAt() {
 }
 
 function DatabaseStatus() {
-  const { isLoading, data } = useSWR(
-    "http://localhost:3000/api/v1/status",
-    fetchAPI,
-    {
-      refreshInterval: 2000,
-    },
-  );
+  const { isLoading, data } = useSWR("api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
 
   let databaseVersion = "Carregando...";
   let databaseMaxConnctions = "Carregando...";
