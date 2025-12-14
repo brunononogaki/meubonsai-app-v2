151a1c9a6878fc5e698c4eb311ba8f6a01c0bf3f
Bruno Nonogaki <brunono@gmail.com>
2025-12-14 09:07:25 -0300

    style: run lint in delete.test.js

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -8,8 +8,8 @@ beforeAll(async () => {
 describe("DELETE /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Getting pending migrations", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/migrations",{
-        method: 'DELETE'
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
       });
       expect(response.status).toBe(405);
       const responseBody = await response.json();
