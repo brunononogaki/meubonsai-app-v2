1a2e7dda3b0fab40cab8661f35822a53b414bfd7
Bruno Nonogaki <brunono@gmail.com>
2025-12-20 23:12:13 -0300

    feat: create users

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -10,6 +10,6 @@ export default router.handler(controller.errorHandler);
 
 async function getHandler(request, response) {
   const username = request.query.username;
-  const userFound = await user.findOneByUsername(username)
+  const userFound = await user.findOneByUsername(username);
   return response.status(200).json(userFound);
 }
