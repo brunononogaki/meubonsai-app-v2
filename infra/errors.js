bb781c2dea51708134c8793351d2b42b1f5bb6ef
Bruno Nonogaki <brunono@gmail.com>
2025-12-20 23:30:13 -0300

    feat: add users

--- infra/errors.js
+++ infra/errors.js
@@ -77,3 +77,25 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super("Não foi possível encontrar esse recurso no sistema", {
+      cause: cause,
+    });
+    this.name = "NotFoundError";
+    this.action =
+      action || "Verifique se os parâmetros enviados na consulta estão certos.";
+    this.statusCode = 404;
+    this.message = message;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
