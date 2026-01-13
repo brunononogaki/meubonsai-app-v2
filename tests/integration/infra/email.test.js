2725a725fa0ae76798b6ccde91b4d3c239eb8dc6
Bruno Nonogaki <brunono@gmail.com>
2026-01-13 18:38:39 -0300

    feat: add email test

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -1,4 +1,10 @@
 import email from "infra/email.js";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.deleteAllEmails();
+  await orchestrator.waitForAllServices();
+});
 
 describe("Test infra/email.js", () => {
   test("send()", async () => {
@@ -9,5 +15,18 @@ describe("Test infra/email.js", () => {
       text: "Text de corpo",
       // html:
     });
+
+    await email.send({
+      from: "MeuBonsai <contato@meubonsai.app>",
+      to: "contato@brunononogaki.com",
+      subject: "Último email enviado",
+      text: "Text de corpo",
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+    expect(lastEmail.sender).toBe("<contato@meubonsai.app>");
+    expect(lastEmail.recipients[0]).toBe("<contato@brunononogaki.com>");
+    expect(lastEmail.subject).toBe("Último email enviado");
+    expect(lastEmail.text).toBe("Text de corpo\n");
   });
 });
